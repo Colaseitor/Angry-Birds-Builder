4940935f13a370e7fddbb2b7d8da57c8bd2209b6
Colaseitor <colaslafuente@gmail.com>
2026-09-20 05:13:36 +0200

    Title screen fixes + bomb

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,4 +1,19 @@
 extends Node2D
+var birds = [
+	"res://Scenes/TitleScreenBirds/title_screen_red.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_chuck.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_laser_bird.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_the_blues.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_terence.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_red_fuji_tv.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_terence_rio_2.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_shakira.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_tony.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_hockey_bird.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_chuck_pikachu.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_matilda.tscn",
+	"res://Scenes/TitleScreenBirds/title_screen_bomb.tscn"
+]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,31 +43,34 @@ func _process(_delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
-	var random_bird = randi_range(1,12)
-	if random_bird == 1:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_red.tscn").instantiate())
-	elif random_bird == 2:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_chuck.tscn").instantiate())
-	elif random_bird == 3:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_laser_bird.tscn").instantiate())
-	elif random_bird == 4:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_the_blues.tscn").instantiate())
-	elif random_bird == 5:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_terence.tscn").instantiate())
-	elif random_bird == 6:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_red_fuji_tv.tscn").instantiate())
-	elif random_bird == 7:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_terence_rio_2.tscn").instantiate())
-	elif random_bird == 8:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_shakira.tscn").instantiate())
-	elif random_bird == 9:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_tony.tscn").instantiate())
-	elif random_bird == 10:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_hockey_bird.tscn").instantiate())
-	elif random_bird == 11:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_chuck_pikachu.tscn").instantiate())
-	elif random_bird == 12:
-		add_child(load("res://Scenes/TitleScreenBirds/title_screen_matilda.tscn").instantiate())
+	add_child(load(birds[randi_range(0, birds.size() - 1)]).instantiate())
+	#var random_bird = randi_range(1,12)
+	#if random_bird == 1:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_red.tscn").instantiate())
+	#elif random_bird == 2:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_chuck.tscn").instantiate())
+	#elif random_bird == 3:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_laser_bird.tscn").instantiate())
+	#elif random_bird == 4:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_the_blues.tscn").instantiate())
+	#elif random_bird == 5:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_terence.tscn").instantiate())
+	#elif random_bird == 6:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_red_fuji_tv.tscn").instantiate())
+	#elif random_bird == 7:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_terence_rio_2.tscn").instantiate())
+	#elif random_bird == 8:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_shakira.tscn").instantiate())
+	#elif random_bird == 9:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_tony.tscn").instantiate())
+	#elif random_bird == 10:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_hockey_bird.tscn").instantiate())
+	#elif random_bird == 11:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_chuck_pikachu.tscn").instantiate())
+	#elif random_bird == 12:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_matilda.tscn").instantiate())
+	#elif random_bird == 13:
+		#add_child(load("res://Scenes/TitleScreenBirds/title_screen_bomb.tscn").instantiate())
 
 
 func _on_load_button_down() -> void:
